ffd6c6ed0f6936e0cdb14215f634424a3ff93e93
unknown <saruxworkz3@gmail.com>
2022-04-07 11:16:31 +0530

    my new file

---
+++ saaaa/Bag.java
@@ -0,0 +1,19 @@
+class Bag
+{
+	 static String Brand;
+	 static String Quality;
+	 static int Price;
+	 static String Color;
+	 static String Type;
+	 
+static void details()
+{
+	System.out.println("bag details");
+	
+    System.out.println(Brand);
+	System.out.println(Quality);
+	System.out.println(Price);
+	System.out.println(Color);
+	System.out.println(Type);	
+}
+}
