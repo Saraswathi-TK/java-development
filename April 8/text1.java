2f944f4cf7c31ea4d6ccf11be8419cbd80035520
unknown <saruxworkz3@gmail.com>
2022-04-08 13:46:06 +0530

    text

---
+++ April 8/text1.java
@@ -0,0 +1,148 @@
+1) How can you create constants in java?
+     create constants in java can use static keyword
+
+2)How many type of datatype are there in java?
+     Two type of datatype in java
+	 1.primitive datatype
+	 2.non-primitive datatype
+	  
+	  primitive are
+	  int byte  char short long double float boolean
+	  
+	  non-primitive are
+	   string array
+	   
+3)Why is specifying datatype mandatory in java?
+     mandatory datatype it is a strongly typed language.
+
+4)what are some common naming convention for java variables?
+    Always start with lowercase letter and captitalized in anothervariables
+  
+
+5)what is variable initialization? how is it different than assigment?
+    variable initialization is initial  the value to assign is called variable initialization 
+	
+
+6)what is array?
+    A container object that holds a fixed values of single type is called array.
+
+7)can you pass the negative number in array size?
+   array cannont have a negative number.
+ 
+8)where dose array stored in jvm memory?
+    Array stored in heap memory.
+
+9) What are the advantages of array?
+  Array can be used for sorting data elements.
+
+10)what is an anonymous array in java? give example?
+	
+	
+11)Write a program to print elements of array?
+   
+  class Array
+  {
+	  String[] name;
+	  
+	  Array(Strin name)
+	  {
+		  System.out.println(name);
+	  }
+	  
+	  class ArrayRunner
+	  {
+		  public static void main(String[] args)
+		  {
+			  Array array=new Array(chandu);
+		  }
+	  }
+  }
+
+12)Write a program to sort an array in java?
+ class Array
+ {
+	 String name;
+	 {
+		 
+	 }
+ }
+ 
+13)write a Program to check whether two given Arrays are equal,given both contains same data type and same length?
+
+
+14)write a program to throw ArrayOutOfBoundsException?
+   class Item
+   {
+	   String name;
+	   
+   }   
+	 
+	 class ItemRunner
+	 {
+		 public static void main(String item)
+		 {
+			 Item item=new Item();
+			 item=new Item(name);
+		 }
+	 }
+	 
+15)what will happen if you do not initialize an array?
+      Array automatically be initialize null values
+
+
+16)find out smallest and largest number in a given array?
+
+
+17)what is static keyword,static variable,static methods in java?
+        static keyword is particular member ,class variable, methods is instanc of class
+ 
+
+18)can we overload and override static methods in java?
+      overload the static methods in java.
+
+19)why main()	method is declared as static?
+   jvm invoke it without  the class.
+
+
+20)what is a static block?
+     set of instraction.
+	
+21)can constructor be static in java?
+      constructor connat be used in static in java	
+ 
+22)can interface in java have static method in it?
+
+
+23)what is constructor chaining?
+    constructor chaining is the join the constructor method
+
+24)what is constructor?
+   A special method that is used to initialize objects.
+
+25)what happens if you keep a return type for a constructor?
+      The constructor connt be run return type is go back the method.
+	  
+26)can we call sub class constructor from super class constructor?
+     call the sub class constructor
+
+27)how can create a instance of child class with parent type?
+      useing extends keyword can create a instance of child class witn parent type.
+
+28)difference between instance and class?
+
+29)how many ways instance be created?
+     two ways be created
+	 reference
+	 methods
+	 
+30)what happens if you create a instance of a class?	 
+	  
+	  
+	  
+ 
+	 
+	
+	
+
+   
+	   
